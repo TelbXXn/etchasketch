167339d7902c2f4bd6e52223fe576b48134fced9
TelbXXn <106255232+TelbXXn@users.noreply.github.com>
2024-03-20 17:04:42 +0100

    added css and changed logic

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 //First create an input that stores in a variable for giving the option to decide grid size
+let newGrids = [];
+let gridSize = 4; //default size 4x4; 
 
 const gridInput = document.querySelector('#gridsize');
+const board = document.querySelector('#playground');
 
 gridInput.addEventListener('click', () => {
     gridSize = +prompt('What size grid would you like?', );
+    createGrid(gridSize);
+
 });
 
-let gridSize = 6;
+
+function createGrid(gridSize) {
+    board.innerHTML = '';
+    newGrids = [];
+    for (i = 0; i < gridSize*gridSize; i++) {        
+        newGrids[i] = document.createElement("div");
+        newGrids[i].classList.add('grids');
+        board.appendChild(newGrids[i]);
+    }
+}
+
+createGrid(gridSize);
